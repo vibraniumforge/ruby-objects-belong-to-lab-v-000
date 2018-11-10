70d39ab73bbb9de8b948fb56cfad0b961612e694
Greg Morgan <gregm9999@gmail.com>
2018-11-10 21:54:52 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,3 @@
 class Author
 attr_accessor :name
-
-def initialize
-  @name=name
-end
-
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,4 @@
 class  Post
   attr_accessor :title,  :author
 
-  def initialize
-    @title=title
-    @author=author
-  end
-
 end
